ec346d54bed0fbcbe138cc051f8cacb03cf75824
mylesfn <mylesfn@sky.com>
2018-11-20 09:00:34 +0000

    Add files via upload

--- TestBasketWebAPI/DataAccess/BasketDbContext.cs
+++ TestBasketWebAPI/DataAccess/BasketDbContext.cs
@@ -20,13 +20,16 @@ namespace TestBasketWebAPI.DataAccess
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-                builder.Entity<Basket>().Property(p => p.BasketId).ValueGeneratedOnAdd();
-                builder.Entity<Customer>().Property(p => p.CustomerId).ValueGeneratedOnAdd();
-                builder.Entity<Product>().Property(p => p.ProductId).ValueGeneratedOnAdd();
-                
-                builder.Entity<Customer>().HasData(new Customer() { CustomerId = -1, FirstName = "Site", LastName = "Owner", Email = "site.owner@basketweb.api", SessionExpiry = DateTime.Now.AddYears(10) });
-                builder.Entity<Basket>().HasData(new Basket() { BasketId = -1, CustomerId = -1, TotalPrice = 0 });
-                builder.Entity<TrustedDomain>().HasData(new TrustedDomain() { Domain = "localhost", AcceptRequests = true });
+            builder.Entity<Basket>().Property(p => p.BasketId).ValueGeneratedOnAdd();
+            builder.Entity<Customer>().Property(p => p.CustomerId).ValueGeneratedOnAdd();
+            builder.Entity<Product>().Property(p => p.ProductId).ValueGeneratedOnAdd();
+
+            builder.Entity<Customer>().HasData(new Customer() { CustomerId = -1, FirstName = "Site", LastName = "Owner", Email = "site.owner@basketweb.api", SessionExpiry = DateTime.Now.AddYears(10) });
+            builder.Entity<Basket>().HasData(new Basket() { BasketId = -1, CustomerId = -1, TotalPrice = 0 });
+
+            //TrustedDomains can be used to accept and reject requests from different domains. If this DbSet contains no records then all requests will
+            //be accepted, however if it contains entries then only requests coming from the TrustedDomain.Domain will be allowed through
+            //builder.Entity<TrustedDomain>().HasData(new TrustedDomain() { Domain = "localhost", AcceptRequests = true });
         }
 
         object _lock = new object();
